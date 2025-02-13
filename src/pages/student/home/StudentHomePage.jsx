df87284acae2f10afb68777295fe21d35db6482e
Jdexurbano <dexterurbano25@gmail.com>
2025-02-13 12:14:37 +0800

    change the name from courses to activities

--- src/pages/student/home/StudentHomePage.jsx
+++ src/pages/student/home/StudentHomePage.jsx
@@ -39,7 +39,7 @@ function StudentHomePage() {
         style={{ scrollbarWidth: "none" }}
       >
         <h1 className="text-3xl sm:text-4xl font-bold text-purple-700 mb-6">
-          Courses
+          Activities
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {course.map((data) => {
